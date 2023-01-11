c94e64b8c56e48e0b58669e2d9fbcbd0f07b8243
maxhanauske <114517513+SunC0der@users.noreply.github.com>
2023-01-11 17:15:18 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,8 @@ import pathlib
 import sys
 #sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, pathlib.Path(__file__))
+sys.path.insert(0, pathlib.Path(__file__)).parents[1].resolve().as_posix())
+sys.path.insert(0, pathlib.Path(__file__)).parents[2].resolve().as_posix())
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
